5d13f5ca1bc3a8d5fd9d451aef6e6c903806b6ec
TRemigi <tayremigi@gmail.com>
2025-02-24 18:02:10 -0700

    Customize starship
    - Also removes duplicate setting in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,7 +13,6 @@ opt.number = true
 opt.cursorline = true
 opt.cursorlineopt = "number"
 
-
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
@@ -22,14 +21,11 @@ opt.expandtab = true -- expand tab to spaces
 opt.smartindent = true
 opt.swapfile = false
 opt.incsearch = true
-
 opt.wrap = false
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-
-opt.cursorline = true
 
 -- turn on termguicolors for tokyonight colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
